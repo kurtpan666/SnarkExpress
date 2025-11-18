f41055f27dcb640761d3bad2106c60a4acd577cc
Kurt Pan <74859307+kurtpan666@users.noreply.github.com>
2025-11-18 11:35:23 +0800

    Update Footer.tsx

--- paper-aggregator/frontend/src/components/Footer.tsx
+++ paper-aggregator/frontend/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className="fixed bottom-4 left-4 text-xs text-gray-600">
-      <div>by the <a href="https://zkpunk.pro" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">ZKPunk</a></div>
+      <div>by  the <a href="https://zkpunk.pro" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">ZKPunk</a></div>
       <div>for the <a href="https://zkpunk.pro" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">ZKPunk</a></div>
     </footer>
   );
